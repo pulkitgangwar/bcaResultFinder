4c75289c5a3b3bb29ada6f4505923760cadc6bdb
pulkitgangwar <pulkitgangwar2016@gmail.com>
2021-02-26 20:58:04 +0530

    updated main.js with chakra-ui

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,122 +1,65 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
-import Button from "@material-ui/core/Button";
-import Menu from "@material-ui/core/Menu";
-import MenuItem from "@material-ui/core/MenuItem";
-import { makeStyles } from "@material-ui/core/styles";
-import BookIcon from "@material-ui/icons/Book";
+import {
+  Flex,
+  Heading,
+  MenuButton,
+  Spacer,
+  Box,
+  Menu,
+  Button,
+  MenuList,
+  MenuItem,
+} from "@chakra-ui/react";
 
-const useStyles = makeStyles(theme => ({
-  root: {
-    flexGrow: 1
-  },
-  menuButton: {
-    marginRight: theme.spacing(2)
-  },
-  title: {
-    flexGrow: 1
-  },
-  headerLink: {
-    color: "white",
-    textDecoration: "none"
-  },
-  link: {
-    color: "black",
-    textDecoration: "none"
-  }
-}));
-
-const Main = props => {
-  const classes = useStyles();
-  const [anchorEl, setAnchorEl] = React.useState(null);
-
-  const handleClick = event => {
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
-
+const Main = (props) => {
   return (
-    <div className={classes.root}>
-      <AppBar position="static">
-        <Toolbar>
-          <BookIcon />
-          <Typography className={classes.title} variant="h3" noWrap>
-            <Link to="/" className={classes.headerLink}>
-              BCA Results
-            </Link>
-          </Typography>
-          <div>
-            <Button
-              aria-controls="menu"
-              aria-haspopup="true"
-              onClick={handleClick}
-              className={classes.headerLink}
-            >
+    <Box>
+      <Flex p="3" backgroundColor="teal" alignItems="center">
+        <Box>
+          <Heading color="white">
+            <Link to="/">BCA Results</Link>
+          </Heading>
+        </Box>
+        <Spacer />
+        <Box>
+          <Menu>
+            <MenuButton as={Button} backgroundColor="white" color="black">
               All Results
-            </Button>
-            <Menu
-              id="menu"
-              anchorEl={anchorEl}
-              keepMounted
-              open={Boolean(anchorEl)}
-              onClose={handleClose}
-            >
-              <MenuItem onClick={handleClose}>
-                <Link className={classes.link} to="/results/2019/1">
-                  2019 FIRST SEM
-                </Link>
+            </MenuButton>
+            <MenuList>
+              <MenuItem>
+                <Link to="/results/2019/1">2019 FIRST SEM</Link>
               </MenuItem>
-              <MenuItem onClick={handleClose}>
-                <Link className={classes.link} to="/results/2018/3">
-                  2018 THIRD SEM
-                </Link>
+              <MenuItem>
+                <Link to="/results/2018/3">2018 THIRD SEM</Link>
               </MenuItem>
-              <MenuItem onClick={handleClose}>
-                <Link className={classes.link} to="/results/2018/2">
-                  2018 SECOND SEM
-                </Link>
+              <MenuItem>
+                <Link to="/results/2018/2">2018 SECOND SEM</Link>
               </MenuItem>
-              <MenuItem onClick={handleClose}>
-                <Link className={classes.link} to="/results/2018/1">
-                  2018 FIRST SEM
-                </Link>
+              <MenuItem>
+                <Link to="/results/2018/1">2018 FIRST SEM</Link>
               </MenuItem>
-              <MenuItem onClick={handleClose}>
-                <Link className={classes.link} to="/results/2017/5">
-                  2017 FIFTH SEM
-                </Link>
+              <MenuItem>
+                <Link to="/results/2017/5">2017 FIFTH SEM</Link>
               </MenuItem>
-              <MenuItem onClick={handleClose}>
-                <Link className={classes.link} to="/results/2017/4">
-                  2017 FOURTH SEM
-                </Link>
+              <MenuItem>
+                <Link to="/results/2017/4">2017 FOURTH SEM</Link>
               </MenuItem>
-              <MenuItem onClick={handleClose}>
-                <Link className={classes.link} to="/results/2017/3">
-                  2017 THIRD SEM
-                </Link>
+              <MenuItem>
+                <Link to="/results/2017/3">2017 THIRD SEM</Link>
               </MenuItem>
-              <MenuItem onClick={handleClose}>
-                <Link className={classes.link} to="/results/2016/6">
-                  2016 SIXTH SEM
-                </Link>
+              <MenuItem>
+                <Link to="/results/2016/6">2016 SIXTH SEM</Link>
               </MenuItem>
-              <MenuItem onClick={handleClose}>
-                <Link className={classes.link} to="/results/2016/5">
-                  2016 FIFTH SEM
-                </Link>
+              <MenuItem>
+                <Link to="/results/2016/5">2016 FIFTH SEM</Link>
               </MenuItem>
-            </Menu>
-          </div>
-        </Toolbar>
-      </AppBar>
-    </div>
+            </MenuList>
+          </Menu>
+        </Box>
+      </Flex>
+    </Box>
   );
 };
 
